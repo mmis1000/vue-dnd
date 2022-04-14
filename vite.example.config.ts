961c43acd7aa49421d9c7326ee3372925766a22f
Mmis1000 <mmis1000@yahoo.com.tw>
2022-04-14 11:20:31 +0800

    Add exmaple build config

---
+++ vite.example.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJSx from '@vitejs/plugin-vue-jsx'
+import typescript from '@rollup/plugin-typescript'
+import * as path from 'path'
+
+const resolvePath = (str: string) => path.resolve(__dirname, str)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJSx()],
+  build: {
+    outDir: './dist-example'
+  }
+})
