728aa98ce32b487f857510a4ce1eaf688ffa03a0
Mmis1000 <mmis1000@yahoo.com.tw>
2022-04-15 23:59:39 +0800

    Tidy example

--- src/components/ExampleHandle.vue
+++ src/components/ExampleHandle.vue
@@ -1,90 +1,96 @@
 <script lang="tsx">
-import { defineComponent, PropType, reactive, computed} from 'vue';
-import { useDraggableWithHandle, useDroppable } from '../packages/vue-dnd';
-import { useHtmlProvider } from '../packages/vue-dnd/use-html-provider';
+import { defineComponent, PropType, reactive, computed } from "vue";
+import { useDraggableWithHandle, useDroppable } from "../packages/vue-dnd";
+import { useHtmlProvider } from "../packages/vue-dnd/use-html-provider";
 
-type Message = [currentBucket: number, id: number]
+type Message = [currentBucket: number, id: number];
 
 const Ball = defineComponent({
   props: {
     currentBucket: {
       type: Number,
-      required: true
+      required: true,
     },
     id: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
-  setup (props) {
+  setup(props) {
     const { wrap, wrapHandle } = useDraggableWithHandle<Message>(
       // Specify what did you want to send to the dropzone, can either be ref or raw value
-      computed(() => [props.currentBucket, props.id] as Message), 
+      computed(() => [props.currentBucket, props.id] as Message),
       {}
-    )
+    );
     return () => {
-      const node = wrap(<div class="box" >
-      {
-        wrapHandle(<div class="box-content">
-          Handle
-        </div>)
-      }
-      { props.id }
-      </div>)
-      return node
-    }
-  }
-})
+      return wrap(
+        <div class="box">
+          {wrapHandle(<div class="box-content">Handle</div>)}
+          {props.id}
+        </div>
+      );
+    };
+  },
+});
 
 const Bucket = defineComponent({
   props: {
     id: {
       type: Number,
-      required: true
+      required: true,
     },
     onDrop: {
-      type: Function as PropType<(item: number, oldBucket: number, newBucket: number) => void>,
-      required: true
-    }
+      type: Function as PropType<
+        (item: number, oldBucket: number, newBucket: number) => void
+      >,
+      required: true,
+    },
   },
-  setup (props, ctx) {
+  setup(props, ctx) {
     const { wrap, hoverState } = useDroppable<Message>({
       // Declare whether you want to receive the draggable item or not
       // A predicate function or raw value
       accept: (msg) => msg[0] !== props.id,
       // The drop zone receives message from draggable item
       onDrop: (ev, data) => {
-        props.onDrop(data[1], data[0], props.id)
-      }
-    })
-    return () => wrap(<div class={ hoverState.hover ? 'bucket hover' : 'bucket'}>{ ctx.slots.default?.() }</div>)
-  }
-})
+        props.onDrop(data[1], data[0], props.id);
+      },
+    });
+    return () =>
+      wrap(
+        <div class={hoverState.hover ? "bucket hover" : "bucket"}>
+          {ctx.slots.default?.()}
+        </div>
+      );
+  },
+});
 
 export default {
-  setup () {
-    useHtmlProvider()
+  setup() {
+    useHtmlProvider();
 
-    const buckets = reactive(new Array(5).fill(null).map(() => [] as number[]))
-    buckets[0].push(1, 2, 3, 4, 5)
+    const buckets = reactive(new Array(5).fill(null).map(() => [] as number[]));
+    buckets[0].push(1, 2, 3, 4, 5);
 
     const onDrop = (id: number, oldBucket: number, newBucket: number) => {
-      console.log(id, oldBucket, newBucket)
-      buckets[oldBucket] = buckets[oldBucket].filter(i => i !== id)
-      buckets[newBucket] = buckets[newBucket].concat([id])
-    }
+      console.log(id, oldBucket, newBucket);
+      buckets[oldBucket] = buckets[oldBucket].filter((i) => i !== id);
+      buckets[newBucket] = buckets[newBucket].concat([id]);
+    };
 
-    return () => <div class="board">
-      {
-        buckets.map((b, index) => <Bucket id={index} key={index} onDrop={onDrop}>
-          {
-            b.map((i, index2) => <Ball id={i} currentBucket={index} key={index2} />)
-          }
-        </Bucket>)
-      }
-    </div>
-  }
-}
+    return () => (
+      <div class="board">
+        {buckets.map((b, index) => (
+          <Bucket id={index} key={index} onDrop={onDrop}>
+            {b.map((i, index2) => (
+              <Ball id={i} currentBucket={index} key={index2} />
+            ))}
+          </Bucket>
+        ))}
+      </div>
+    );
+  },
+};
 </script>
 <style scoped>
 .board {
