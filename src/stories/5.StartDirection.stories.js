15ae88f320e6ef03833738e25f4320daf26aa562
Mmis1000 <mmis1000@yahoo.com.tw>
2022-05-17 17:47:35 +0800

    Add start direction story

---
+++ src/stories/5.StartDirection.stories.js
@@ -0,0 +1,54 @@
+import StartDirection from '../components/StartDirection';
+import startDirectionSource from '!../components/StartDirection.vue?raw';
+import startDirectionSourceDraggable from '!../components/startDirection/StartDirectionDraggable.vue?raw';
+export default {
+  title: 'VueDnd/StartDirection',
+  component: StartDirection,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    usePointerEvent: {
+      type: { name: 'boolean', required: false },
+      defaultValue: false,
+      description: 'whether using the native drag and drop of emulated pointer events based drag and drop',
+    },
+    startDirection: {
+      type: { required: false },
+      options: ['all', 'x', 'y'],
+      defaultValue: 'all',
+      description: 'valid start direction',
+    },
+  },
+  parameters: {
+    componentSource: {
+      code: [
+        startDirectionSource,
+        startDirectionSourceDraggable,
+      ],
+      language: 'html',
+    }
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { StartDirection },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<start-direction v-bind="args" />',
+});
+
+export const NativeEvents = Template.bind({});
+NativeEvents.args = {
+  // More on composing args: https://storybook.js.org/docs/vue/writing-stories/args#args-composition
+  usePointerEvent: false
+};
+
+export const PointerEvents = Template.bind({});
+PointerEvents.args = {
+  // More on composing args: https://storybook.js.org/docs/vue/writing-stories/args#args-composition
+  usePointerEvent: true
+};
